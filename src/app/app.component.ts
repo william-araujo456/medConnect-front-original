1bdcc0f0d733f418bfbbfa2d8943e9796865334b
William Mattson Kheel Gomes Araújo <william.mattson02@gmail.com>
2025-09-10 21:04:14 -0400

    múltiplas instalações npm e edições no código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {MatButton} from '@angular/material/button';
+import {NgClass} from '@angular/common';
+
+interface Menu {
+  title: string;
+  route: string;
+  isCurrent?: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class AppComponent {
-  title = 'medconnect-front';
+  public menuList: Menu[] = [
+    {title: 'Pacientes', route: '/patients', isCurrent: false},
+    {title: 'Médico', route: '/medics', isCurrent: false},
+    {title: 'Consultas', route: '/consultations', isCurrent: false}
+  ]
+  public router: Router = new Router();
+  constructor() {
+    this.changeMenu(this.menuList[0]);
+  }
+
+  public changeMenu(item: Menu): void {
+    const menu = this.menuList.forEach(
+      (menu) => menu.isCurrent = menu.route === item.route
+    );
+    this.goToPage(item.route)
+  }
+
+  public goToPage(route: string): void {
+    this.router.navigate([route]).then();
+  }
 }
